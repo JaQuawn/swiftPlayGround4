80e736d2469db295cf0274f2617451c8f99683fe
jaquawn <jd23@hawaii.edu>
2018-02-23 16:11:40 -1000

    finished new playgrounds

--- swiftPlayGround4.playground/Contents.swift
+++ swiftPlayGround4.playground/Contents.swift
@@ -1,5 +1,29 @@
 //: Playground - noun: a place where people can play
 
 import UIKit
+// for number in 1...5
+//{
+    //print(number)
+//}
 
-var str = "Hello, playground"
+
+for number in 1...5
+{
+    print(number)
+}
+
+var sum = 0
+for number in 1...5
+{
+    sum += number
+    print(sum)
+}
+
+
+var i = 1
+
+while  i <= 10
+{
+    print(i)
+    i = i+1
+}
